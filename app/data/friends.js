058511e8a6e2fa431bb85730e2717913a6865c83
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-04-23 15:42:38 -0700

    updates var

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,5 @@
- var friends= [{  
+ var friends = [
+   {  
    "name":"Ahmed",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
@@ -13,7 +14,23 @@
         4,
         1
       ]
-    }
+    },
+    {  
+      "name":"Person2",
+       "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+       "scores":[
+           2,
+           2,
+           4,
+           1,
+           3,
+           2,
+           3,
+           1,
+           1,
+           3
+         ]
+       }
   ];
 
   module.exports = friends;
