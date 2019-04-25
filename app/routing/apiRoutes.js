6c33c97cfa50dda32e38bc8a8b610b0245f74738
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-04-24 21:43:16 -0700

    route troubleshooting

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,9 @@
 var friends = require("../data/friends.js");
 
 module.exports = function(app){
-  app.get('/api/friends', function(req, res) {
-      res.json(friends);
-    });
 
   app.post('/api/friends', function(req, res) {
+    res.send(req.body);
     var totalDifference = 0;
     var bestMatch = {
       name: "",
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,5 +14,4 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname + "/../public/home.html"));
   });
 
-
 };
